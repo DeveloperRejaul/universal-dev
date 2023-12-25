26258575989ff87b3653522522ca5fc2bcfe7ea4
rezaul <rezaul_karim@coredevs.ltd>
2023-12-25 19:21:30 +0600

    add

--- src/app/(stack)/components.tsx
+++ src/app/(stack)/components.tsx
@@ -15,7 +15,16 @@ export default function Components() {
        {/* Display Slider components  */}
        <View style={{rowGap:5}}>
         <Component.H3 className='text-center'> Slider components </Component.H3>
-        <Component.Slider/>
+        <Component.Slider 
+          thumbTintColor={"black"}
+          maximumTrackTintColor={"#ff0000"} 
+          minimumTrackTintColor={"green"}
+          maximumValue={100}
+          minimumValue={0} 
+          onValueChange={(val)=>{console.log(val)}}
+          onSlidingComplete={()=>{console.log("Sliding complete")}}
+          onSlidingStart={()=>{console.log("Sliding start")}}
+          />
        </View>
 
 
