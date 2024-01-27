1d186ccd9c475833db38678732e4f2ab5e7193b8
DeveloperRejaul <devrejaul.official@gmail.com>
2024-01-27 21:28:59 +0600

    pwa

--- __tests__/src/app/index.test.ts
+++
@@ -1,4 +0,0 @@
-// Snapshot Test
-import React from 'react';
-import renderer from 'react-test-renderer';
-
---
+++ src/assets/icon/MenuIcon.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Path, Svg } from 'react-native-svg';
+
+export default function MenuIcon() {
+  return (
+    <Svg width='27' height='22' viewBox='0 0 27 22' fill='none'>
+      <Path d='M2 2H25' stroke='#CA9A62' strokeWidth='2.5' strokeLinecap='round' />
+      <Path d='M2 11H25' stroke='#CA9A62' strokeWidth='2.5' strokeLinecap='round' />
+      <Path d='M2 20H25' stroke='#CA9A62' strokeWidth='2.5' strokeLinecap='round' />
+    </Svg>
+  );
+}
